ad881e5e5dbdefeb1fd0372de703f267ad177a01
setsuna <setsuna@wo.cn>
2018-08-30 23:44:58 +0800

    index

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="app">
+        <Page/>
+    </div>
+</template>
+<script>
+    import Page from './components/Page/Page.vue'
+    export default {
+        components: {
+            Page
+        }
+    }
+</script>
+<style lang="less">
+    @import '../static/css/reset.css';
+    @import '../static/css/swiper.css';
+    html,
+    body {
+        width: 100%;
+        height: 100%;
+    }
+    .app {
+        width: 100%;
+        height: 100%;
+        background: url(../static/img/timg.jpg) no-repeat center center;
+        background-size: 100% 100%;
+    }
+</style>
